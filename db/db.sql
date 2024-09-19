9c0db9722bd971ab8f15a5de7ddaab46ae803fbc
WVII <andrewmakuisa1@gmail.com>
2024-09-19 19:40:40 +0200

    complete sql table

--- db/db.sql
+++ db/db.sql
@@ -13,3 +13,32 @@ CREATE TABLE commentaire {
     id_client CHAR(30) NOT NULL,
     CONSTRAINT commentaire_id_client_fk FOREIGN KEY (id_client) REFERENCES client(id_client)
 };
+
+CREATE TABLE reservation {
+    date_r DATE NOT NULL,
+    id_client VARCHAR(30) NOT NULL,
+    id_sejour VARCHAR(30) NOT NULL,
+    CONSTRAINT reservation_id_client_fk FOREIGN KEY (id_client) REFERENCES client(id_client),
+    CONSTRAINT reservation_id_sejour_fk FOREIGN KEY (id_sejour) REFERENCES sejour(id_sejour)
+};
+
+CREATE TABLE activite {
+    id_activite INT NOT NULL PRIMARY KEY,
+    ref_activite VARCHAR(100) NOT NULL,
+    jour_activite DATE NOT NULL,
+    heure_activite TIME NOT NULL,
+    duree_activite INT NOT NULL
+}
+
+CREATE TABLE aime {
+    id_client VARCHAR(30) NOT NULL,
+    id_activite VARCHAR(30) NOT NULL,
+    CONSTRAINT reservation_id_client_fk FOREIGN KEY (id_client) REFERENCES client(id_client),
+    CONSTRAINT reservation_id_activite_fk FOREIGN KEY (id_activite) REFERENCES activite(id_activite)
+}
+
+CREATE TABLE mail {
+    id_mail VARCHAR(30) NOT NULL,
+    contenu VARCHAR(255) NOT NULL,
+    date_mail DATE NOT NULL
+}
