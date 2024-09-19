9b92e8db985851d81d30832b82b6d90885be8ccb
WVII <andrewmakuisa1@gmail.com>
2024-09-19 17:36:21 +0200

    second update

--- db/db.sql
+++ db/db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE client {
-    id_c INT NOT NULL PRIMARY KEY,
+    id_client INT NOT NULL PRIMARY KEY,
     nom VARCHAR(30) NOT NULL,
     prenom VARCHAR(30) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -8,6 +8,8 @@ CREATE TABLE client {
 
 CREATE TABLE commentaire {
     id_com INT NOT NULL PRIMARY KEY,
-    date_com DATE NOT NULL PRIMARY KEY,
-    id_c 
-}
+    date_com DATE NOT NULL CHECK(Date_com <= CURRENT_DATE),
+    note VARCHAR(100) NOT NULL,
+    id_client CHAR(30) NOT NULL,
+    CONSTRAINT commentaire_id_client_fk FOREIGN KEY (id_client) REFERENCES client(id_client)
+};
